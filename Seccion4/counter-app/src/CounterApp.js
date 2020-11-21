f4a19787ea31f9577d942839b2188dc12cb3ed70
DarkPrometheus <61800713+DarkPrometheus@users.noreply.github.com>
2020-11-21 00:31:36 -0800

    Seccion 4/Ejercicio 1

---
+++ Seccion4/counter-app/src/CounterApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from "prop-types"
+
+const CounterApp = ( { value } ) => {
+    
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{ value }</h2>
+        </>
+    );
+}
+
+CounterApp.protoTypes = {
+    value: PropTypes.number
+}
+
+export default CounterApp;
+
+// raftcp
--- Seccion4/counter-app/src/index.js
+++ Seccion4/counter-app/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
 import "./index.css"
+import CounterApp from './CounterApp';
 
 ReactDOM.render(
-    <PrimeraApp saludo="Hola, mundo"/>,
+    <CounterApp value={ 738 }/>,
     document.querySelector("#root")
 );
